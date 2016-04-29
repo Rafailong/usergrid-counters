f26ce9cb7871a0d523d75f7aa396743defd30813
Rafael Avila <rafa.avim@gmail.com>
2016-04-29 16:09:11 -0500

    refactor at post functions

--- src/functions.js
+++ src/functions.js
@@ -36,27 +36,20 @@ export function getCounter (request, counterName = validations.required('counter
     .then((response) => response.count)
 }
 
-export function incrementCounter (request,
-  counterName = validations.required('counterName', counterName),
-  increment = validations.validIncrement('increment', increment = 1)) {
-
+export function incrementCounter (request, counterName = validations.required('counterName', counterName) , increment = validations.validIncrement('increment', increment = 1)) {
   let body = { 'timestamp': 0, 'counters': {} }
   body.counters[counterName] = increment
-  const reqOpts = Object.assign(createEventsRequestBaseOptions, { body })
-  return request(reqOpts)
-    .then((response) => {
-      let entities = response.entities
-      if (_.isEmpty(entities)) { return undefined }
-      return entities[0].counters
-    })
+  return postRequestWrapper(body, request)
 }
 
-export function resetCounter (request,
-  counterName = validations.required('counterName', counterName)) {
-
+export function resetCounter (request, counterName = validations.required('counterName', counterName)) {
   let body = { 'timestamp': 0, 'counters': {} }
   body.counters[counterName] = 0
-  const reqOpts = Object.assign(createEventsRequestBaseOptions, { body })
+  return postRequestWrapper(body, request)
+}
+
+function postRequestWrapper (body, request) {
+  const reqOpts = Object.assign(createEventsRequestBaseOptions, { body})
   return request(reqOpts)
     .then((response) => {
       let entities = response.entities
